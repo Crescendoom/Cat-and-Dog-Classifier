4948aae96159e2b2666c0c0c0efebbbc0921fc56
Laurence Hernandez <enzohernandez65@gmail.com>
2025-05-05 01:54:13 +0800

    Minor changes

--- test_classifier.py
+++ test_classifier.py
@@ -4,14 +4,32 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from classifier import model, target_size
+import shutil
 
 class TestClassifierWithRealImages(unittest.TestCase):
-    def setUp(self):
-        # Path to test images
-        self.test_dir = os.path.join("test_set")
-        self.cat_dir = os.path.join(self.test_dir, "cats")
-        self.dog_dir = os.path.join(self.test_dir, "dogs")
+    @classmethod
+    def setUpClass(cls):
+        # Check if test directories exist, if not create them
+        cls.test_dir = os.path.join("test_set")
+        cls.cat_dir = os.path.join(cls.test_dir, "cats")
+        cls.dog_dir = os.path.join(cls.test_dir, "dogs")
         
+        if not os.path.exists(cls.test_dir):
+            os.makedirs(cls.test_dir, exist_ok=True)
+            os.makedirs(cls.cat_dir, exist_ok=True)
+            os.makedirs(cls.dog_dir, exist_ok=True)
+            print(f"Created test directories at {cls.test_dir}")
+            print("Please add cat images to test_set/cats/ and dog images to test_set/dogs/")
+            print("Then run the test again.")
+            raise unittest.SkipTest("Test directories created. Add images and run again.")
+
+        # Check if there are images in the directories
+        if len(os.listdir(cls.cat_dir)) == 0 or len(os.listdir(cls.dog_dir)) == 0:
+            print("No images found in test directories.")
+            print("Please add cat images to test_set/cats/ and dog images to test_set/dogs/")
+            raise unittest.SkipTest("No test images found. Add images and run again.")
+
+    def setUp(self):
         # Get a few sample images
         self.cat_images = [os.path.join(self.cat_dir, img) for img in os.listdir(self.cat_dir)[:3]]
         self.dog_images = [os.path.join(self.dog_dir, img) for img in os.listdir(self.dog_dir)[:3]]
